acd1e37bea5b34fa0917aca4e60cd2a2fec0eed5
VladimirKuzmych <vladimirkuzmych@gmail.com>
2021-09-07 22:46:09 +0300

    time intervals lazy routing

--- src/app/modules/time-intervals/components/index.ts
+++ src/app/modules/time-intervals/components/index.ts
@@ -5,3 +5,8 @@ export const timeIntervalsComponents = [
     TimeIntervalsContainerComponent,
     TimeIntervalSelectComponent,
 ];
+
+export {
+    TimeIntervalSelectComponent,
+    TimeIntervalsContainerComponent,
+};
--- src/app/modules/time-intervals/time-intervals.module.ts
+++ src/app/modules/time-intervals/time-intervals.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { timeIntervalsComponents } from './components';
+import { RouterModule } from '@angular/router';
+import { timeIntervalRoutes } from './time-intervals.routes';
 
 
 @NgModule({
     declarations: [...timeIntervalsComponents],
     imports: [
         CommonModule,
+        RouterModule.forChild(timeIntervalRoutes),
     ],
     exports: [...timeIntervalsComponents],
 })
